b06321567ed296b10578065c504caa4c68f1c05b
Javier Uncos <javiersrfc30@gmail.com>
2022-08-02 19:43:48 -0300

    se realizo el ejercicio 7 ,quedaron 3 metodos sin realizar

--- js/tp4-ejer7.js
+++ js/tp4-ejer7.js
@@ -13,7 +13,7 @@ buscarContacto(nombre): busca un contacto por su nombre y muestra su teléfono.
 eliminarContacto(Contacto c): elimina el contacto de la agenda, indica si se ha eliminado o no por pantalla
 agendaLlena(): indica si la agenda está llena.
 huecosLibres(): indica cuántos contactos más podemos ingresar.*/
-let agenda = []
+let agenda = [];
 
 let contacto;
 
@@ -25,9 +25,12 @@ class Agenda {
 
   agregarContacto() {
     for (let i = 0; i < 10; i++) {
-       nuevoContacto = new Agenda (prompt('ingresa un nombre'),parseInt(prompt('ingresa un numero de telfono')))
+      nuevoContacto = new Agenda(
+        prompt("ingresa un nombre"),
+        parseInt(prompt("ingresa un numero de telfono"))
+      );
 
-       agenda.push(nuevoContacto)
+      agenda.push(nuevoContacto);
 
       if (confirm("desea agregar  mas contatos ?")) {
       } else {
@@ -35,38 +38,100 @@ class Agenda {
       }
       if (agenda.length >= 10) {
         alert("agenda llena , no puedes agregar mas contactos");
-        break
+        break;
       }
     }
     agenda.sort();
-    console.log(agenda)
+    console.log(agenda);
   }
 
-  contactoExiste(){
-   
+  contactoExiste() {
+
   }
 
-  listarContacto(){
-       for(let i = 0; i< agenda.length;i++){
-          document.write(Object.values(agenda[i]))
-       }
+  listarContacto() {
+    for (let i = 0; i < agenda.length; i++) {
+      document.write(`
+          <ul>
+          <li>${Object.values(agenda[i])}</li>
+          </ul>
+          `);
+    }
+  }
+
+  buscarContacto(){
+
+  }
+
+  eliminarContacto(){
+
+  }
+
+  agendaLlena(){
+    if(agenda.length <= 10){
+      document.write("la agenda esta llena")
+    }
+  }
+
+
+  huecosLibre(){
+   switch(true){
+    case agenda.length === 0:
+      document.write("quedan 10 espacios en tu agenda")
+      break
+    case agenda.length === 1:
+      document.write("quedan 9 espacios en tu agenda")
+      break
+    case agenda.length === 2:
+      document.write("quedan 8 espacios en tu agenda")
+      break
+    case agenda.length === 3:
+      document.write("quedan 7 espacios en tu agenda")
+      break
+    case agenda.length === 4:
+      document.write("quedan 6 espacios en tu agenda")
+      break
+    case agenda.length === 5:
+      document.write("quedan 5 espacios en tu agenda")
+      break
+    case agenda.length === 6:
+      document.write("quedan 4 espacios en tu agenda")
+      break
+    case agenda.length === 7:
+      document.write("quedan 3 espacios en tu agenda")
+      break
+    case agenda.length === 8:
+      document.write("quedan 2 espacios en tu agenda")
+      break
+    case agenda.length === 9:
+      document.write("quedan 1 espacios en tu agenda")
+      break
     }
     
-      get regresarNombre(){
-       return this.nombre
-      }
+    
+  }
+  set cambiarNombre(valor){
+    this.nombre= valor;
+  }
+  get regresarNombre() {
+    return this.nombre;
   }
 
+  set cambiarTelefono(valor){
+    this.telefono = valor;
+  }
+
+  get mostrasTelefono(){
+    return this.telefono;
+  }
+}
   
-
-
-
-
-
-
 let nuevoContacto = new Agenda(
   prompt("ingresa un nombre"),
   parseInt(prompt("ingresa un numero de telefono"))
 );
 agenda.push(nuevoContacto);
 
+
+
+// Nota : todos los metodos son ingresados por consola
